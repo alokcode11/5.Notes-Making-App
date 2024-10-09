1ba403da87125a7a776bede33f652d8427372d41
alokcode11 <alokbabu911@gmail.com>
2024-10-10 05:00:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+// can i use json stringify on notes
+function updateStorage () {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/edit.png";
+    
+    // Append img to inputBox, then append inputBox to notesContainer
+    inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox);
+  });
+
+  
+// createBtn.addEventListener("click", () => {
+//   let inputBox = document.createElement("p");
+//   let img = document.createElement("img");
+//   inputBox.className = "input-box";
+//   inputBox.setAttribute("contenteditable", "true");
+//   img.src = "images/delete.png";
+//   notesContainer.appendChild(inputBox).appendChild("img");
+// });
+//doubt
+notesContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function (){
+                updateStorage();
+            }
+        })
+    }
+    document.addEventListener("keydown", event => {
+        if (event.key === "Enter") {
+            document.execCommand("insertLineBreak");
+            event.preventDefault();
+        }
+    })
+});
